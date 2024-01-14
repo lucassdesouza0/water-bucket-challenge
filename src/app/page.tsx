d008f650031ef6d9b304a5ad063ce0d965ab15a4
Lucas Souza <lucassouza@MacBook-Air-de-Marina.local>
2024-01-14 01:40:04 -0300

    feat: no solution message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,22 @@
 import Form from "@/components/Form";
 import WaterJug from "@/components/WaterJug";
 import { useActionsContext } from "./contexts/actions";
-import jiggleSolver from "@/services/waterJugSolver";
+import waterJugSolver from "@/services/waterJugSolver";
+import { useState } from "react";
 
 export default function Home() {
+	const [noSolution, setNoSolution] = useState(false);
 	const { actions, jugCapacity, setJugCapacity, setActions } =
 		useActionsContext();
 
 	const handleFormSubmit = (jugX: number, jugY: number, target: number) => {
 		setJugCapacity({ jugX, jugY });
-		const result = jiggleSolver(jugX, jugY, target);
+		const result = waterJugSolver(jugX, jugY, target);
 		if (result) {
+			setNoSolution(false);
 			setActions(result);
+		} else {
+			setNoSolution(true);
 		}
 	};
 
@@ -22,10 +27,18 @@ export default function Home() {
 			<h1 className="text-5xl text-gray-800 font-normal">WATER JUG RIDDLE</h1>
 
 			<Form onFormSubmit={handleFormSubmit} />
-			<div className="flex flex-row gap-8">
-				<WaterJug isXJug capacity={jugCapacity} actions={actions} />
-				<WaterJug capacity={jugCapacity} actions={actions} />
-			</div>
+			{noSolution ? (
+				<div className="w-100 h-60 p-8">
+					<p className="rounded-lg bg-red-500 text-white font-bold p-4">
+						No solution found
+					</p>
+				</div>
+			) : (
+				<div className="flex flex-row gap-8">
+					<WaterJug isXJug capacity={jugCapacity} actions={actions} />
+					<WaterJug capacity={jugCapacity} actions={actions} />
+				</div>
+			)}
 		</div>
 	);
 }
